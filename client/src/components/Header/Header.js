d869c52f7337d3ca1fdb8d4b68724ae9030170f0
Kristen Fairman <fairman.kristen@gmail.com>
2018-12-05 10:41:32 -0600

    new

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return(
+    <div className="jumbotron container">
+      <h1>New York Times Article Scrubber</h1>
+      <hr/>
+      <h4>Search Articles of Interest.</h4>
+    </div>
+  )
+};
+
+export default Header;
